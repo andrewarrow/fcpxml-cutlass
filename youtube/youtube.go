17ba72f2c915bc48ecfaf93aaf27168af2c29675
aa <oneone@gmail.com>
2025-06-10 00:14:02 -0700

    put video ids back with mov

--- youtube/youtube.go
+++ youtube/youtube.go
@@ -22,7 +22,7 @@ func DownloadVideo(youtubeID string) (string, error) {
 	cmd := exec.Command("yt-dlp",
 		"-o", "./data/"+mp4File,
 		"--restrict-filenames",
-		"--exec", "ffmpeg -i {} {}.mov && rm {}",
+		"--exec", fmt.Sprintf("ffmpeg -i {} %s.mov && rm {}", youtubeID),
 		youtubeID)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
