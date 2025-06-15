64706382cd28756611e6e084b23fdaad2511225d
aa <oneone@gmail.com>
2025-06-15 10:22:07 -0700

    run working

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@ rm filename.fcpxml
 ./cutlass fcp add-image ./assets/yammer.com.png --duration 20 -o filename.fcpxml
 ./cutlass fcp add-slide 0 --input filename.fcpxml -o filename.fcpxml
 ./cutlass fcp add-text slide_text.txt --input filename.fcpxml -o filename.fcpxml
+./cutlass fcp add-image ./assets/bitium.com.png --input filename.fcpxml -o filename.fcpxml
+./cutlass fcp add-slide 20 --input filename.fcpxml -o filename.fcpxml
+./cutlass fcp add-text slide_text.txt --offset 21 --input filename.fcpxml -o filename.fcpxml
 
 
