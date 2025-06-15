aa831045fbd996bf1ec8e2bc648868f8fa86fcff
aa <oneone@gmail.com>
2025-06-14 20:23:53 -0700

    adding timestamp

--- cmd/fcp.go
+++ cmd/fcp.go
@@ -3,17 +3,31 @@ package cmd
 import (
 	"cutlass/fcp"
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var fcpCmd = &cobra.Command{
-	Use:   "fcp [filename.fcpxml]",
+	Use:   "fcp",
 	Short: "Generate an empty FCPXML file from structs",
 	Long:  `Generate a basic empty FCPXML file structure using the fcp package structs.`,
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		filename := args[0]
+		// Get output filename from flag or generate default
+		output, _ := cmd.Flags().GetString("output")
+		var filename string
+		
+		if output != "" {
+			filename = output
+		} else if len(args) > 0 {
+			filename = args[0]
+		} else {
+			// Generate default filename with unix timestamp
+			timestamp := time.Now().Unix()
+			filename = fmt.Sprintf("cutlass_%d.fcpxml", timestamp)
+		}
+		
 		_, err := fcp.GenerateEmpty(filename)
 		if err != nil {
 			fmt.Printf("Error generating FCPXML: %v\n", err)
@@ -24,13 +38,24 @@ var fcpCmd = &cobra.Command{
 }
 
 var addVideoCmd = &cobra.Command{
-	Use:   "add-video [filename.fcpxml] [video-file]",
+	Use:   "add-video [video-file]",
 	Short: "Add a video to an FCPXML file using structs",
 	Long:  `Add a video asset and asset-clip to an FCPXML file using the fcp package structs.`,
-	Args:  cobra.ExactArgs(2),
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		filename := args[0]
-		videoFile := args[1]
+		videoFile := args[0]
+		
+		// Get output filename from flag or generate default
+		output, _ := cmd.Flags().GetString("output")
+		var filename string
+		
+		if output != "" {
+			filename = output
+		} else {
+			// Generate default filename with unix timestamp
+			timestamp := time.Now().Unix()
+			filename = fmt.Sprintf("cutlass_%d.fcpxml", timestamp)
+		}
 		
 		// Generate empty FCPXML structure
 		fcpxml, err := fcp.GenerateEmpty("")
@@ -58,5 +83,11 @@ var addVideoCmd = &cobra.Command{
 }
 
 func init() {
+	// Add output flag to main fcp command
+	fcpCmd.Flags().StringP("output", "o", "", "Output filename (defaults to cutlass_unixtime.fcpxml)")
+	
+	// Add output flag to add-video subcommand
+	addVideoCmd.Flags().StringP("output", "o", "", "Output filename (defaults to cutlass_unixtime.fcpxml)")
+	
 	fcpCmd.AddCommand(addVideoCmd)
 }
