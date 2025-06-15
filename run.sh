7a2d4c5057f0dc764b829222e4beb5f018095396
aa <oneone@gmail.com>
2025-06-15 10:14:44 -0700

    run working great

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 go build
 rm filename.fcpxml
-./cutlass fcp filename.fcpxml
-./cutlass fcp add-video filename.fcpxml ./assets/speech1.mov
+./cutlass fcp add-image ./assets/yammer.com.png --duration 20 -o filename.fcpxml
+./cutlass fcp add-slide 0 --input filename.fcpxml -o filename.fcpxml
+./cutlass fcp add-text slide_text.txt --input filename.fcpxml -o filename.fcpxml
+
 
